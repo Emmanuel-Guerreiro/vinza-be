a92bea2cdd8f5d33e7abbf8d42d8ebced793e1e1
NinoPassera <cuentapasseran@gmail.com>
2025-08-11 00:44:20 -0300

    Eliminacion de instacia evento

--- src/instancia-evento/types.ts
+++
@@ -1,4 +0,0 @@
-import { InstanciaEventoCreationAttributes } from './model';
-
-export type CreateInstanciaEventoDto = InstanciaEventoCreationAttributes;
-export type UpdateInstanciaEventoDto = Partial<CreateInstanciaEventoDto>;
